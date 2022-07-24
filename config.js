790b9ba8c52326db5ba89faf24bb2c4290bcfee2
NdaaXD <98279739+NdaaXD@users.noreply.github.com>
2022-07-24 18:36:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,11 +43,11 @@ global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
 
 //========Url Template Buttons=========//
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/aguzfamilia"
+global.urlnya = "https://www.instagram.com"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 882-3992-8939'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
